9d9342b193bc1a07b0cad38612d9dfa94ee63534
Vyacheslav <ResSlavkaRes@mail.ru>
2022-10-07 17:07:12 +0300

    init

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 import TelegramApi from 'node-telegram-bot-api'
 import TOKEN from './env/env.js';
+
 const token = TOKEN;
+const webAppUrl = 'https://endearing-bienenstitch-05d481.netlify.app'
 
 const bot = new TelegramApi(token, {polling: true});
 
-const start = async () => {
-    await bot.setMyCommands([
-        {command: '/start', description: 'Начало'}
-    ])
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
 
+    if (text === '/start') {
+        await bot.sendMessage(chatId, 'ниже кнопка', {
+            reply_markup: {
+                keyboard: [
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+                ]
+            }
+        })
 
-}
+        await bot.sendMessage(chatId, 'ниже кнопка', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
+    }
+})
 
-start()
